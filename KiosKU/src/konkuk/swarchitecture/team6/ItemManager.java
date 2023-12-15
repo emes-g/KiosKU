541544017e1ba6bba3d6e9a54b51a3d2202cd19c
emes-g <saltishDev@gmail.com>
2023-12-15 16:38:51 +0900

    feat : ItemManager

--- KiosKU/src/konkuk/swarchitecture/team6/ItemManager.java
+++ KiosKU/src/konkuk/swarchitecture/team6/ItemManager.java
@@ -15,16 +15,72 @@ public class ItemManager {
 		return itemList;
 	}
 	
-	public boolean addItem(Item item) {
-		// 내부 나중에
+	public boolean addItem() {
+		String title = inputItemTitle();
+		int cost = inputItemCost();
+		Item item = new Item(title, cost);
+		
+		if(isExistedItem(title))
+			return false;
+		
+		itemList.add(item);
 		return true;
 	}
-	public boolean editItem(int num) {	// num == index
-		// 내부 나중에
+	public boolean editItem() {
+		int idx = inputItemIdx();
+		Item item = itemList.get(idx);
+		
+		while(true) {
+			String title = inputItemTitle();
+			
+			if(!isExistedItem(title)) {
+				item.setTitle(inputItemTitle());
+				break;
+			}
+			System.out.printf("중복된 이름입니다. 다른 이름을 입력해주세요\n");
+		}
+		item.setCost(inputItemCost());
+		
 		return true;
 	}
-	public boolean deleteItem(int num) {
-		// 내부 나중에
+	public boolean deleteItem() {
+		int idx = inputItemIdx();
+		
+		itemList.remove(idx);
+		
 		return true;
+	}
+	public int inputItemIdx() {
+		int idx;
+
+		System.out.printf("상품 번호 : ");
+		idx = Kiosk.scan.nextInt();
+		Kiosk.clearBuffer();
+
+		return idx;
+	}
+	public String inputItemTitle() {
+		String title;
+		
+		System.out.printf("상품명 : ");
+		title = Kiosk.scan.nextLine();
+		
+		return title;
+	}
+	public int inputItemCost() {
+		int cost;
+
+		System.out.printf("상품 가격 : ");
+		cost = Kiosk.scan.nextInt();
+		Kiosk.clearBuffer();
+
+		return cost;
+	}
+	public boolean isExistedItem(String title) {
+		for(Item it : itemList) {
+			if(it.getTitle().equals(title))
+				return true;
+		}
+		return false;
 	}
 }
