d550a7027506bc832d3bc16639f80b8a908ec769
emes-g <saltishDev@gmail.com>
2023-12-15 16:07:36 +0900

    feat : delete Customer, Owner's code

--- KiosKU/src/konkuk/swarchitecture/team6/Customer.java
+++ KiosKU/src/konkuk/swarchitecture/team6/Customer.java
@@ -1,19 +1,5 @@
 package konkuk.swarchitecture.team6;
 
-public class Customer {
-	private Receipt receipt;
-	
+public class Customer {	
 	public Customer() {}
-	
-	public String printReceiptInfo() {
-		// 추후 구현
-		String test = "";
-		return test;
-	}
-	
-	public String receiveChange(int[] change) {
-		// 추후 구현
-		String test = "";
-		return test;
-	}
 }
--- KiosKU/src/konkuk/swarchitecture/team6/Owner.java
+++ KiosKU/src/konkuk/swarchitecture/team6/Owner.java
@@ -1,13 +1,5 @@
 package konkuk.swarchitecture.team6;
 
 public class Owner {
-	private Order order;
-
 	public Owner() {}
-	
-	public String printOrderInfo() {
-		// 추후 구현
-		String test = "";
-		return test;
-	}
 }
