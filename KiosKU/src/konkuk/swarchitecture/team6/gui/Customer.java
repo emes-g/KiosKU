348a74ba4a5f7345d0a4207e483c9a4f9df3e6e9
Minseok <93303181+emes-g@users.noreply.github.com>
2023-12-16 14:42:48 +0900

    delete : delete gui directory for making a new branch

--- KiosKU/src/konkuk/swarchitecture/team6/gui/Customer.java
+++
@@ -1,5 +0,0 @@
-package konkuk.swarchitecture.team6.gui;
-
-public class Customer {	
-	public Customer() {}
-}
--- KiosKU/src/konkuk/swarchitecture/team6/gui/PaymentManagerIF.java
+++
@@ -1,5 +0,0 @@
-package konkuk.swarchitecture.team6.gui;
-
-public interface PaymentManagerIF {
-	public abstract boolean pay();
-}
